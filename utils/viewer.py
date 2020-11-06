0de733f32d4dd06c54bdd8a213ef562ffa3a81c7
rezenter <nisovru@gmail.com>
2020-11-06 23:38:12 +0300

    viewer update for big files

--- utils/viewer.py
+++ utils/viewer.py
@@ -1,142 +1,132 @@
 import matplotlib.pyplot as plt
 import json
 import statistics
+import os
+import ijson
+import math
 
 path = 'd:/data/fastDump/debug/'
-group_count = 8
+HEADER_FILE = 'header'
+FILE_EXT = 'json'
+_group_count = 8
 _ch_count = 2
 
-front_threshold = 500  # mV
-
-boards = [0]  # boards to be processed
-channels = [1]  # channels to be processed
-invert = []  # channels to be inverted
+board_idx = 0  # boards to be processed
+channels = [1, 2]  # channels to be processed
+prehistory_ns = 125  # ns
 
 shot_filename = "shotn.txt"
 with open(shot_filename, 'r') as shotn_file:
     line = shotn_file.readline()
     shotn = int(line)
 
-print(shotn)
-shotn = 283
+print('Last shot #%d' % shotn)
+shotn = 275  # manual override
+print('Viewing shot #%d' % shotn)
 shot_folder = '%s%05d' % (path, shotn)
 
+channels.sort()
+props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 
-def find_rising(signal, is_trigger):
-    if is_trigger:
-        local_threshold = trigger_threshold
-        local_threshold = 130
-    else:
-        local_threshold = front_threshold
+
+def read_file(board_id):
+    if not os.path.isdir(shot_folder):
+        print('Requested shotn is missing.')
+        return {}
+    if not os.path.isfile('%s/%d.%s' % (shot_folder, board_id, FILE_EXT)):
+        print('Requested shot is missing requested board file.')
+        return {}
+    with open('%s/%d.%s' % (shot_folder, board_id, FILE_EXT), 'rb') as board_file:
+        return [event['groups'] for event in ijson.items(board_file, 'item', use_float=True)]
+
+
+def find_rising(signal, threshold):
     for i in range(len(signal) - 1):
-        if signal[i + 1] >= local_threshold > signal[i]:
-            return i + (local_threshold - signal[i]) / (signal[i + 1] - signal[i])
-        if signal[i + 1] <= -local_threshold < signal[i]:
-            return i - (local_threshold + signal[i]) / (signal[i + 1] - signal[i])
+        if signal[i + 1] >= threshold > signal[i]:
+            return i + (threshold - signal[i]) / (signal[i + 1] - signal[i])
     return -1
+
+
+def find_falling(signal, threshold):
+    for i in range(len(signal) - 1):
+        if signal[i] >= threshold > signal[i + 1]:
+            return i + (signal[i] - threshold) / (signal[i] - signal[i + 1])
+    return len(signal)
 
 
 with open('%s/header.json' % shot_folder, 'r') as header:
     data = json.load(header)
-    #board_count = len(data['boards'])
     freq = float(data['frequency'])  # GS/s
     time_step = 1 / freq  # nanoseconds
-    event_len = data['eventLength']
+    timeline_prototype = [time_step * cell_index for cell_index in range(data['eventLength'])]
     trigger_threshold = data['triggerThreshold']
-    timeline_prototype = [0]
-    while len(timeline_prototype) != event_len:
-        timeline_prototype.append(timeline_prototype[-1] + time_step)
+    prehistory_length = int(prehistory_ns // time_step) + 1
 
-shifted = {}
-for board_idx in boards:
-    with open('%s/%d.json' % (shot_folder, board_idx), 'r') as board_file:
-        data = json.load(board_file)
+
+
+    print('Reading file...')
+    data = read_file(board_idx)
     print('board %d recorded %d events' % (board_idx, len(data)))
-    ax1 = plt.subplot(len(boards), 1, boards.index(board_idx) + 1)
-    shifted[board_idx] = []
+    fig, ax = plt.subplots()
+    processed_data = []
+
     for event in data:
+        front = find_rising(event[0]['data'][0], trigger_threshold)
         shifted_event = {
-            'timeline': [],
+            'timeline': [timeline_prototype[i] - front * time_step for i in range(len(timeline_prototype))],
             'channels': {},
-            'fronts': {}
+            'zero': [],
+            'std': [],
+            'max': [],
+            'fwhm': [],
+            'delay': []
         }
-        local_timeline = timeline_prototype.copy()
-        for group_idx in range(group_count):
+        for group_idx in range(_group_count):
             for ch_idx in range(_ch_count):
                 ch_num = ch_idx + group_idx * 2
                 if ch_num not in channels:
                     continue
+                zero = statistics.fmean(event[group_idx]['data'][ch_idx][:prehistory_length])
                 shifted_event['channels'][ch_num] = []
-                if ch_num == 0:
-                    front = find_rising(event['groups'][group_idx]['data'][ch_idx], True)
-                    #front = find_rising(event['groups'][2]['data'][0], True)
-                    for i in range(len(local_timeline)):
-                        local_timeline[i] -= front * time_step
-                        shifted_event['timeline'].append(local_timeline[i])
-                    shifted_event['fronts'][ch_num] = front * time_step
+                maximum = 0
+                for i in range(len(shifted_event['timeline'])):
+                    shifted_event['channels'][ch_num].append(event[group_idx]['data'][ch_idx][i] - zero)
+                    maximum = max(maximum, shifted_event['channels'][ch_num][-1])
+                plt.plot(shifted_event['timeline'], shifted_event['channels'][ch_num], alpha=0.1)
+                shifted_event['zero'].append(zero)
+                shifted_event['std'].\
+                    append(statistics.stdev(event[group_idx]['data'][ch_idx][:prehistory_length], zero))
+                shifted_event['max'].append(maximum)
+                shifted_event['delay'].append(find_rising(event[group_idx]['data'][ch_idx], maximum / 2.0))
+                shifted_event['fwhm'].\
+                    append(find_falling(event[group_idx]['data'][ch_idx][math.floor(shifted_event['delay'][-1]):],
+                                        maximum / 2.0))
+        processed_data.append(shifted_event)
 
 
-                    """print('min = %.3f, max = %.3f' %
-                          (min(event['groups'][group_idx]['data'][ch_idx]),
-                           max(event['groups'][group_idx]['data'][ch_idx])))
-"""
-                else:
-                    front = find_rising(event['groups'][group_idx]['data'][ch_idx], False)
-                    shifted_event['fronts'][ch_num] = front * time_step
-                for i in range(len(local_timeline)):
-                    if ch_num in invert:
-                        shifted_event['channels'][ch_num].append(-event['groups'][group_idx]['data'][ch_idx][i])
-                    else:
-                        shifted_event['channels'][ch_num].append(event['groups'][group_idx]['data'][ch_idx][i])
-                plt.plot(local_timeline, shifted_event['channels'][ch_num])
 
-        shifted[board_idx].append(shifted_event)
-        plt.ylabel('signal, mV')
-        plt.xlabel('timeline, ns')
-        plt.title('ADC %d' % board_idx)
+    plt.ylabel('signal, mV')
+    plt.xlabel('timeline, ns')
+    plt.title('ADC %d (starting from 0)' % board_idx)
+    plt.xlim(-50, 100)
+    plt.ylim(-100, 1000)
+    filename = '../figures/board%d_ch%s' % (board_idx, channels)
+    plt.savefig('%s.png' % filename)
+
+    with open('%s.csv' % filename, 'w') as out_file:
+        out_file.write('ch, zero, std, prehistory_std, std, max, std, fwhm, std, delay, std\n')
+        for ch_ind in range(len(channels)):
+            line = '%02d, ' % channels[ch_idx]
+            tmp = [event['zero'][ch_idx] for event in processed_data]
+            line += '%.5f, ' % statistics.fmean(tmp)
+
+            tmp = [event['max'][ch_idx] for event in processed_data]
+            print(ch_idx, tmp[0])
+
+            line += '%.5f, ' % statistics.fmean(tmp)
+            out_file.write('%s\n' % line)
+    print('OK')
+    plt.show()
 
 
-'''
-print('writing file...')
-with open('%s/shifted.csv' % shot_folder, 'w') as shifted_file:
-    line = ''
-    for board_idx in boards:
-        line += 't, '
-        for ch in channels:
-            line += 'b%dch%d, ' % (board_idx, ch)
-    shifted_file.write(line[:-2] + '\n')
-    for event in range(len(shifted[boards[0]])):
-        for cell_idx in range(len(timeline_prototype)):
-            line = ''
-            for board_idx in boards:
-                line += '%.4f, ' % shifted[board_idx][event]['timeline'][cell_idx]
-                for ch in channels:
-                    line += '%.2f, ' % shifted[board_idx][event]['channels'][ch][cell_idx]
-            shifted_file.write(line[:-2] + '\n')
 
-with open('%s/fronts.csv' % shot_folder, 'w') as fronts_file:
-    line = ''
-    for board_idx in boards:
-        for ch in channels:
-            line += 'b%dch%d, ' % (board_idx, ch)
-    fronts_file.write(line[:-2] + '\n')
-    for event in range(len(shifted[boards[0]])):
-        line = ''
-        for board_idx in boards:
-            for ch in channels:
-                line += '%.2f, ' % shifted[board_idx][event]['fronts'][ch]
-        fronts_file.write(line[:-2] + '\n')
-
-for board_idx in boards:
-    print("board %d", board_idx)
-    for ch in channels:
-        if ch == 0:
-            continue
-        dt = [shifted[board_idx][event]['fronts'][ch] - shifted[board_idx][event]['fronts'][0]
-              for event in range(len(shifted[boards[0]]))]
-        print("ch %d, mean = %.3f, std = %.2f, ptp = %.2f" %
-              (ch, sum(dt) / len(dt), statistics.stdev(dt), max(dt) - min(dt)))
-              
-'''
-print('OK')
-plt.show()
